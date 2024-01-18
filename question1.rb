1d70f1bf6fe583d94c1dfe52a67c4618ee2919b2
dinnupatel100 <dinnupatel100@gmail.com>
2024-01-18 19:53:49 +0530

    made changes

--- question1.rb
+++ question1.rb
@@ -1,34 +1,28 @@
 #1. Write a program to display number is prime or not
 
-#Program to check the given number is prime or not
 
 #function to check number is prime or not
 def check_prime(number)
   
-    #Condition for values less than 1
-    if(number<1)
-        puts "Enter number greater than 0"
-        return
-    end
-    
-    # Corner case for value 1
-    if(number==1)
-        puts "1 is not a prime number nor a composite number"
-        return
-    end
-
-
-    # loop to check prime number
+  if(number<1)
+    puts "Enter number greater than 0"
+  elsif(number==1)
+    puts "1 is not a prime number nor a composite number"
+  else
     upper_limit = number/2
-    for i in 2..upper_limit do 
+    (2..upper_limit).each do |i|
         if number%i==0
-            puts "#{number} is not a prime number"
-            return
+        puts "#{number} is not a prime number"
+        return
         end
     end
     puts "#{number} is a prime number"
+  end
+
 end
 
 #calling function with number as argument
-check_prime(0)
+print "Enter the number : "
+number = gets.chomp.to_i
+check_prime(number)
 
