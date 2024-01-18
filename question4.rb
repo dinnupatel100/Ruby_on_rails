8589c81bb510e23773f39674cde1cdc5403c79a8
dinnupatel100 <dinnupatel100@gmail.com>
2024-01-18 23:07:29 +0530

    modify question4

--- question4.rb
+++ question4.rb
@@ -1,12 +1,27 @@
 # 4. Regex for mobile number, email address, name, gender(M / F) and amount
 
 text = "My number is 9284282726 my age is 24 amount Rs.6789 email - dineshpatel@gmail.com gender - M"
-number = text.match(/\d{10}/)         # to find mobile number
-amount = text.match(/Rs.\d+/)         # to find amount
-email = text.match(/\w+@\w+.\w+/)     # to find email
-gender = text.match(/[MFmf]/)         # to find gender
 
-puts number
-puts amount
-puts email
-puts gender
+puts "\n1. Print Number\n2. Print Email\n3.Print Amount\n4.Print Gender"
+print "\nEnter the number : "
+number = gets.chomp.to_i
+
+case number
+when 1
+  number = text.match(/\d{10}/)
+  puts number  
+when 2       
+  email = text.match(/\w+@\w+.\w+/)
+  puts email 
+when 3     
+  amount = text.match(/Rs.\d+/) 
+  puts amount
+when 4
+  gender = text.match(/[MFmf]/)
+  puts gender
+end
+
+
+
+
+
